dc51c4e49a252f3c60975effebd17b6709452cfa
Charmaine Chan <chachan@Charmaines-MacBook-Pro.local>
2021-06-14 16:32:09 +0800

    cleaned up the board style

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const numbers = [
   12, 13,
 ];
 const originalPouch = []; // Each tile is repeated, there are 2 duplicated sets
-const playerRack = [];
+const playerRack = []; // true location
+const ontherack = ""
 
 const main = () => {
   // BUILDING POUCH
@@ -13,6 +14,7 @@ const main = () => {
     constructor(colour, number) {
       this.colour = colour;
       this.number = number;
+      this.ontherack = true;
     }
   }
 
@@ -24,7 +26,7 @@ const main = () => {
       }
     }
   };
-  buildPouch();
+ 
 
   // DRAWING RANDOM NUMBER - draws a random card and removes it from the original pouch of 104 tiles
   const drawRandomCard = () => {
@@ -37,53 +39,73 @@ const main = () => {
     const playerMaxCards = 20;
     for (let i = 0; i < playerMaxCards; i++) {
       if (playerRack.length < playerMaxCards) {
-        playerRack.push(drawRandomCard(originalPouch)[0]); // returning the element
+        playerRack.push(drawRandomCard(originalPouch)[0]);
+         // returning the element
       }
     }
   };
 
-  addTileToRack();
 
   // PLAYER TO PUT CARDS OUT ON PLAYTABLE
   // creating the player rack on JQUERY
-
   const $body = $("body");
   const $nameInput = $("<textarea>").attr("id", "nameinput");
+  const $tableFlexBox = $('<div>').attr("id","tableFlexBox")
+  $('#playtable').append($tableFlexBox)
+  const $rackFlexBox = $('<div>').attr("id","rackFlexBox")
+  $('#rack').append($rackFlexBox)
+ 
+
 
   $body.prepend($nameInput);
-
-  // create individual divs for each tile on the player rack
-  // const $playerRack1 = $('<div>').text(playerRack[0].number).addClass(playerRack[0].colour)
-  // $('.row1').append($playerRack1)
-  // const $playerRack2 = $('<div>').text(playerRack[1].number).addClass(playerRack[1].colour)
-  // $('.row1').append($playerRack2)
-
-  // playerrack
-  // [],[],[],[],[],[],[],[],[],[]
-  //
-
-  const startGame = () => {
-    for (let i = 0; i < playerRack.length; i++) {
-      ($("<div>").text(playerRack[i].number).addClass(playerRack[i].colour)).appendTo($(".rack"));
-    }
-  };
-  startGame();
-
-  console.log("player rack is now", playerRack);
-  console.log("player rack array length is now", playerRack.length);
-  console.log("original rack is now", originalPouch);
-  console.log(
-    "original pouch length is now",
-    originalPouch.length,
-    "the length should be 84"
-  );
-
   // PLAYER TO PUT CARDS OUT ON PLAYTABLE
   // append divs with unique id
   // PLAYER TO DRAW CARD IF NEEDED
   // PLAYER TO MAKE COMPLETE SETS ON TABLE AND CLICK BUTTON
   // logic and create button
   // COMPUTER TO CHECK, FOR EACH COMPLETE SET, PLAYER GETS POINTS FROM TOTAL SUM FROM COMPLETED SETS
+
+//   const renderRack = () => {
+//     for (let i = 0; i < playerRack.length; i++) {
+//       ($("<div>").text(playerRack[i].number).addClass(playerRack[i].colour)).appendTo($(".rack"));
+//     }
+//   };
+
+// need to add button
+const renderRack = () => {
+    for (let i = 0; i < playerRack.length; i++) {
+      ($("<div>").text(playerRack[i].number).addClass(playerRack[i].colour).on('click', moveToTable)).appendTo($('#rackFlexBox'));
+    }
+  };
+
+
+const moveToTable = (event) => {
+    $('#tableFlexBox').append($(event.target));
+    $(event.target).on('click', moveToTable)
+    }
+    
+   
+
+
+
+//   $playTable = $('.playtable1')
+//   const $selectTile = $(".black");
+//   $selectTile.on('click', () => {
+//       console.log("works")
+//     $playTable.append($selectTile)
+//   })
+ 
+
+  buildPouch();
+  addTileToRack();
+  renderRack();
+ 
+
+  console.log("player rack is now", playerRack);
+  console.log("player rack array length is now", playerRack.length);
+  console.log("original rack is now", originalPouch);
+  console.log("original pouch length is now", originalPouch.length,"the length should be 84");
+
 };
 
 $(main);
